5e97dccb62521b5b8f2f58347650f8ae2c5b274e
Andrew Lalis <andrew.lalis@freshbooks.com>
2023-05-15 11:34:41 +0200

    Cleaned up tests for transferTo

--- source/streams/functions.d
+++ source/streams/functions.d
@@ -8,36 +8,38 @@ import streams.primitives;
 
 import std.traits;
 
-/**
- * Continously reads an input stream into an in-memory buffer, until 0 elements
- * could be read, or an error occurs.
- * Params:
- *   stream = The stream to read from.
- *   bufferSize = The size of the internal buffer to use for reading.
- * Returns: The full contents of the stream.
- */
-DataType[] readAll(StreamType, DataType)(
-    ref StreamType stream,
-    uint bufferSize = 8192
-) if (isInputStream!(StreamType, DataType)) {
-    import std.array : Appender, appender;
-    Appender!(DataType[]) app = appender!(DataType[])();
-    DataType[] buffer = new DataType[bufferSize];
-    int itemsRead;
-    while ((itemsRead = stream.read(buffer)) > 0) {
-        app ~= buffer[0 .. itemsRead];
+version (D_BetterC) {} else {
+    /**
+     * Continously reads an input stream into an in-memory buffer, until 0 elements
+     * could be read, or an error occurs.
+     * Params:
+     *   stream = The stream to read from.
+     *   bufferSize = The size of the internal buffer to use for reading.
+     * Returns: The full contents of the stream.
+     */
+    E[] readAll(S, E = StreamType!S)(
+        ref S stream,
+        uint bufferSize = 8192
+    ) if (isInputStream!(S, E)) {
+        import std.array : Appender, appender;
+        Appender!(E[]) app = appender!(E[])();
+        E[] buffer = new E[bufferSize];
+        int itemsRead;
+        while ((itemsRead = stream.read(buffer)) > 0) {
+            app ~= buffer[0 .. itemsRead];
+        }
+        return app[];
     }
-    return app[];
-}
 
-unittest {
-    import streams;
-    import std.file;
+    unittest {
+        import streams;
+        import std.file;
 
-    auto s1 = FileInputStream("LICENSE");
-    ulong expectedSize = getSize("LICENSE");
-    ubyte[] data = readAll!(FileInputStream, ubyte)(s1);
-    assert(data.length == expectedSize);
+        auto s1 = FileInputStream("LICENSE");
+        ulong expectedSize = getSize("LICENSE");
+        ubyte[] data = readAll(s1);
+        assert(data.length == expectedSize);
+    }
 }
 
 /** 
@@ -49,15 +51,22 @@ unittest {
  *   output = The output stream to write to.
  *   bufferSize = The size of the internal buffer to use for reading.
  */
-void transferTo(InputStreamType, OutputStreamType, DataType)(
-    ref InputStreamType input,
-    ref OutputStreamType output,
+void transferTo(I, O, E = StreamType!I)(
+    ref I input,
+    ref O output,
     uint bufferSize = 8192
-) if (isInputStream!(InputStreamType, DataType) && isOutputStream!(OutputStreamType, DataType)) {
-    DataType[] buffer = new DataType[bufferSize];
+) if (isInputStream!(I, E) && isOutputStream!(O, E)) {
+    import core.stdc.stdlib : malloc, free;
+    E* bufferPtr = cast(E*) malloc(bufferSize * E.sizeof);
+    if (bufferPtr is null) {
+        throw new Error("Failed to allocate buffer for transfering elements from input stream to output stream.");
+    }
+    scope(exit) {
+        free(bufferPtr);
+    }
     int itemsRead;
-    while ((itemsRead = input.read(buffer)) > 0) {
-        int written = output.write(buffer[0 .. itemsRead]);
+    while ((itemsRead = input.read(bufferPtr[0 .. bufferSize])) > 0) {
+        int written = output.write(bufferPtr[0 .. itemsRead]);
         if (written != itemsRead) {
             throw new StreamException("Failed to transfer bytes.");
         }
@@ -67,27 +76,21 @@ void transferTo(InputStreamType, OutputStreamType, DataType)(
 unittest {
     import streams;
     import std.file;
-    import std.stdio;
 
+    // Check that transferring does indeed work by transferring the LICENSE file to memory.
     auto sIn = FileInputStream("LICENSE");
-    auto sOut = FileOutputStream("LICENSE-COPY");
-    scope(exit) {
-        std.file.remove("LICENSE-COPY");
-    }
-    transferTo!(FileInputStream, FileOutputStream, ubyte)(sIn, sOut);
+    auto sOut = byteArrayOutputStream();
+    transferTo(sIn, sOut);
     sIn.close();
-    sOut.close();
-    assert(getSize("LICENSE") == getSize("LICENSE-COPY"));
+    assert(getSize("LICENSE") == sOut.toArrayRaw().length);
+    assert(cast(ubyte[]) readText("LICENSE") == sOut.toArrayRaw());
 
     // Check that a stream exception is thrown if transfer fails.
-    sIn = FileInputStream("LICENSE");
-    import std.socket;
-    Socket[2] pair = socketPair();
-    auto socketOut = SocketOutputStream(pair[0]);
-    pair[0].close(); // Intentionally close the socket to trigger a write error.
+    auto sIn2 = arrayInputStreamFor!ubyte([1, 2, 3]);
+    auto sOut2 = ErrorOutputStream!ubyte();
     try {
-        transferTo!(FileInputStream, SocketOutputStream, ubyte)(sIn, socketOut);
-        assert(false, "No exception was thrown.");
+        transferTo(sIn2, sOut2);
+        assert(false, "Expected StreamException to be thrown.");
     } catch (StreamException e) {
         // This is expected.
     }
