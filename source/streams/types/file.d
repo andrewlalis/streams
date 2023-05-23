2204833835827dd20d5d5023830bb2982dfd603a
Andrew Lalis <andrewlalisofficial@gmail.com>
2023-05-24 00:34:36 +0200

    Implemented file streams using stdio C functions.

--- source/streams/types/file.d
+++ source/streams/types/file.d
@@ -4,100 +4,164 @@
  */
 module streams.types.file;
 
-import std.stdio : File;
-import core.stdc.stdio;
-
-version (D_BetterC) {} else {
+import core.stdc.stdio : fopen, fclose, fread, fwrite, fflush, feof, ferror, FILE;
 
 /** 
  * A byte input stream that reads from a file. Makes use of the underlying
  * `fopen` and related C functions.
  */
 struct FileInputStream {
-    private File file;
+    private FILE* filePtr;
 
-    this(File file) {
-        this.file = file;
+    this(FILE* filePtr) {
+        assert(filePtr !is null);
+        this.filePtr = filePtr;
     }
 
-    this(string filename) {
-        this(File(filename, "rb"));
+    this(const(char*) filename) {
+        this(fopen(filename, "rb"));
     }
 
     int readFromStream(ubyte[] buffer) {
-        ubyte[] slice = this.file.rawRead(buffer);
-        return cast(int) slice.length;
+        if (this.filePtr is null) {
+            return 0;
+        }
+        size_t bytesRead = fread(buffer.ptr, ubyte.sizeof, buffer.length, this.filePtr);
+        if (bytesRead != buffer.length && ferror(this.filePtr) != 0) {
+            return -1; // cov-ignore
+        }
+        return cast(int) bytesRead;
     }
 
     void closeStream() {
-        if (this.file.isOpen()) {
-            this.file.close();
+        if (this.filePtr !is null) {
+            fclose(this.filePtr);
+            this.filePtr = null;
         }
     }
 }
 
 unittest {
-    import streams.primitives;
+    import streams.primitives : isInputStream, isClosableStream;
+    import streams.types.array : ArrayOutputStream, byteArrayOutputStream;
+    import streams.functions : transferTo;
+    import core.stdc.stdio;
 
     assert(isInputStream!(FileInputStream, ubyte));
     assert(isClosableStream!FileInputStream);
+
+    // Test reading from a file.
+    ArrayOutputStream!ubyte sOut = byteArrayOutputStream();
+    FILE* fp1 = fopen("LICENSE", "rb");
+    fseek(fp1, 0L, SEEK_END);
+    ulong expectedFilesize = ftell(fp1);
+    fclose(fp1);
+    FileInputStream fIn = FileInputStream("LICENSE");
+    transferTo(fIn, sOut);
+    fIn.closeStream();
+    // Check that after closing the stream, the file pointer is nullified.
+    assert(fIn.filePtr is null);
+    ubyte[3] tempBuffer;
+    assert(fIn.readFromStream(tempBuffer) == 0); // Reading after closed should return 0.
+
+    // Check that the number of bytes read matches.
+    assert(sOut.toArrayRaw().length == expectedFilesize);
+
+    // Check that the read was correct manually. We need a no-gc way to read
+    // the file contents without using the FileInputStream impl.
+    import core.stdc.stdlib;
+    fp1 = fopen("LICENSE", "rb");
+    assert(fp1 !is null);
+    ubyte* buffer = cast(ubyte*) malloc(expectedFilesize * ubyte.sizeof);
+    size_t bytesRead = fread(buffer, ubyte.sizeof, expectedFilesize, fp1);
+    assert(bytesRead == expectedFilesize);
+    fclose(fp1);
+
+    assert(sOut.toArrayRaw() == buffer[0 .. expectedFilesize]);
+    free(buffer);
 }
 
 /** 
  * A byte output stream that writes to a file.
  */
 struct FileOutputStream {
-    private File file;
+    private FILE* filePtr;
 
-    this(File file) {
-        this.file = file;
+    this(FILE* filePtr) {
+        assert(filePtr !is null);
+        this.filePtr = filePtr;
     }
 
-    this(string filename) {
-        this(File(filename, "wb"));
+    this(const(char*) filename) {
+        this(fopen(filename, "wb"));
     }
 
     int writeToStream(ubyte[] buffer) {
-        this.file.rawWrite(buffer);
-        return cast(int) buffer.length;
+        if (this.filePtr is null) {
+            return 0;
+        }
+        size_t bytesWritten = fwrite(buffer.ptr, ubyte.sizeof, buffer.length, this.filePtr);
+        if (bytesWritten < buffer.length && ferror(this.filePtr) != 0) {
+            return -1; // cov-ignore
+        }
+        return cast(int) bytesWritten;
     }
 
     void flushStream() {
-        this.file.flush();
+        if (this.filePtr !is null) {
+            fflush(this.filePtr);
+        }
     }
 
     void closeStream() {
-        if (this.file.isOpen()) {
-            this.file.close();
+        if (this.filePtr !is null) {
+            fclose(this.filePtr);
+            this.filePtr = null;
         }
     }
 }
 
 unittest {
-    import streams;
-    import std.file;
-    import std.stdio;
+    import streams.primitives : isOutputStream, isClosableStream, isFlushableStream;
+    import core.stdc.stdio;
+    import core.stdc.stdlib;
+
 
     assert(isOutputStream!(FileOutputStream, ubyte));
     assert(isClosableStream!FileOutputStream);
     assert(isFlushableStream!FileOutputStream);
 
     // Test flushing of file.
-    const FILENAME = "test-file-flush";
-    File f1 = File(FILENAME, "wb");
-    auto sOut = FileOutputStream(f1);
-    sOut.flushStream();
-    assert(getSize(FILENAME) == 0);
-    auto dOut = dataOutputStreamFor(sOut);
-    dOut.writeToStream!(char[5])("Hello");
-    sOut.flushStream();
-    assert(readText(FILENAME) == "Hello");
-    f1.close();
-    try {
-        std.file.remove(FILENAME);
-    } catch (FileException e) {
-        stderr.writefln!"Failed to delete file %s: %s"(FILENAME, e.msg); // cov-ignore
+    const(char*) FILENAME = "test-file-flush";
+    scope(exit) {
+        int result = remove(FILENAME);
+        assert(result == 0);
     }
-}
 
+    FileOutputStream fOut = FileOutputStream(FILENAME);
+    char[5] content = ['H', 'e', 'l', 'l', 'o'];
+    fOut.writeToStream(cast(ubyte[5]) content);
+    
+    // Check that the file doesn't exist yet, when we haven't flushed.
+    FILE* fp1 = fopen(FILENAME, "rb");
+    ubyte* buffer = cast(ubyte*) malloc(1000 * ubyte.sizeof);
+    size_t bytesRead = fread(buffer, ubyte.sizeof, 1000, fp1);
+    assert(bytesRead == 0);
+    assert(feof(fp1) != 0);
+    assert(ferror(fp1) == 0);
+    fclose(fp1);
+
+    // Flush and check that the contents have updated.
+    fOut.flushStream();
+    fp1 = fopen(FILENAME, "rb");
+    assert(fp1 !is null);
+    bytesRead = fread(buffer, ubyte.sizeof, 1000, fp1);
+    assert(bytesRead == 5);
+    fclose(fp1);
+    assert(buffer[0 .. 5] == content);
+
+    // Check that the file pointer is closed upon closing the stream.
+    fOut.closeStream();
+    assert(fOut.filePtr is null);
+    assert(fOut.writeToStream(cast(ubyte[5]) content) == 0);
 }
