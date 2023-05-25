118f7a665ddf6f0694615e942ab882e4cb0774cf
Andrew Lalis <andrew.lalis@freshbooks.com>
2023-05-25 15:56:01 +0200

    Fix value issue.

--- source/streams/types/chunked.d
+++ source/streams/types/chunked.d
@@ -38,7 +38,7 @@ unittest {
 
 private uint writeHexString(uint value, char[] buffer) {
     const(char[]) chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-    if (buffer == 0) {
+    if (value == 0) {
         buffer[0] = '0';
         return 1;
     }
